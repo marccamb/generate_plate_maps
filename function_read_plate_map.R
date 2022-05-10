f9d98890960d10738ae6ff6f2c26ee816553784d
Marine C. Cambon <cambonmarine@gmail.com>
2022-05-10 13:42:51 +0100

    Added automatic plate ID if not provided by user

--- function_read_plate_map.R
+++ function_read_plate_map.R
@@ -1,4 +1,5 @@
-read.plate.map <- function(file_names, plate_id="1", n_wells=96, csv_sep=",", path=".") {
+read.plate.map <- function(file_names, plate_id=NULL, n_wells=96, csv_sep=",", path=".") {
+  if(is.null(plate_id)) plate_id <- seq(1:length(file_names))
   if (length(file_names)==1) {
     f <- read.table(file.path(path, file_names), sep=csv_sep, h=T)
     if(dim(f)[1]*(dim(f)[2]-1)!= n_wells) stop(paste("The plate does not have", n_wells, "columns. Please check the field separator csv_sep, or the number of wells n_wells"))
