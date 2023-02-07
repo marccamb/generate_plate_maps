c99e43909d81a39a1d3c99497ba6d8b2c985ccd0
Marine C. Cambon <cambonmarine@gmail.com>
2023-02-07 19:26:21 +0000

    corrected a bug in the generation of pdf and png outputs

--- print_plate_map.R
+++ print_plate_map.R
@@ -33,11 +33,20 @@ print.plate.map <- function(d, col, txt = NULL, legend.title=NULL, pdf=F, png=F,
   
   l <- unique(substr(d$well, 1, 1))
   c <- unique(gsub("[A-Z]", "", d$well))
+  if(length(d$well)==96) {
+    wi <- 10
+    he <- 6
+  } else {
+    wi <- 5
+    he <- 3
+  }
   
-  i <- ifelse(pdf & png, 1, 2)
-  while (i < 3) {
-    if(i==1 & pdf) pdf(paste0(file.name, ".pdf"), 7,5)
-    if(i==2 & png) png(paste0(file.name, ".png"), 7,5, units = "in", res = 300)
+  id <- 0 
+  if(pdf) id <- c(id,1)
+  if(png) id <- c(id,2)
+  for (i in id) {
+    if(i==1) pdf(paste0(file.name, ".pdf"), wi,he)
+    if(i==2) png(paste0(file.name, ".png"), wi,he, units = "in", res = 300)
     par(bty="n", xpd=T, las=1, mar=c(4,2,2,8),fg="gray30", col.axis="gray30")
     plot(rep(1:length(c), each=length(l)), rep(length(l):1, length(c)), 
          axes=F, xlab="", ylab="",
@@ -56,8 +65,6 @@ print.plate.map <- function(d, col, txt = NULL, legend.title=NULL, pdf=F, png=F,
              unique(names(col))
       )
     }
-    if(i==1 & pdf) dev.off()
-    if(i==2 & png) dev.off()
-    i <- i + 1
+    if(i!=0) dev.off()
   }
 }
