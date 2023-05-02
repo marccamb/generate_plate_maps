1b069c8aa5c5a8a5a7d8a835ffb4140fb439ba9b
Marine C. Cambon <cambonmarine@gmail.com>
2023-05-02 12:01:47 +0100

    Fixed error message for plate reading

--- read_plate_map.R
+++ read_plate_map.R
@@ -29,7 +29,7 @@ read.plate.map <- function(file_names, plate_id=NULL, n_wells=96, csv_sep=",", p
   if(is.null(plate_id)) plate_id <- seq(1:length(file_names))
   if (length(file_names)==1) {
     f <- read.table(file.path(path, file_names), sep=csv_sep, h=T)
-    if(dim(f)[1]*(dim(f)[2]-1)!= n_wells) stop(paste("The plate does not have", n_wells, "columns. Please check the field separator csv_sep, or the number of wells n_wells"))
+    if(dim(f)[1]*(dim(f)[2]-1)!= n_wells) stop(paste("The file does not contain the appropriate culumn number for", n_wells, "well plates. Please check the field separator csv_sep, or the number of wells n_wells"))
     res <- reshape(f, idvar = "row", varying = names(f)[-1], direction = "long")
     res$plate <- plate_id
   } else {
